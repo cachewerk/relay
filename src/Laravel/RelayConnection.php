3ad4a44338eba84ea01e19658b64083238372410
Viktor Szépe <viktor@szepe.net>
2022-06-10 19:40:00 +0000

    Remove mixin from RelayConnection

--- src/Laravel/RelayConnection.php
+++ src/Laravel/RelayConnection.php
@@ -7,9 +7,6 @@ namespace CacheWerk\Relay\Laravel;
 use Illuminate\Contracts\Redis\Connection;
 use Illuminate\Redis\Connections\PhpRedisConnection;
 
-/**
- * @Vmixin \Relay\Relay
- */
 class RelayConnection extends PhpRedisConnection implements Connection
 {
     /**
