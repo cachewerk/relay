046778964fe0cb86f1a686251a9b8c795253ae30
Till Krüss <till@kruss.io>
2022-06-12 14:19:17 -0700

    pass relay method calls directly to class

--- src/Laravel/RelayConnection.php
+++ src/Laravel/RelayConnection.php
@@ -7,96 +7,31 @@ namespace CacheWerk\Relay\Laravel;
 use Illuminate\Contracts\Redis\Connection;
 use Illuminate\Redis\Connections\PhpRedisConnection;
 
+/**
+ * @mixin \Relay\Relay
+ */
 class RelayConnection extends PhpRedisConnection implements Connection
 {
     /**
-     * Dispatch invalidation events.
+     * The Redis client.
      *
-     * @return int|false
+     * @var \Relay\Relay
      */
-    public function dispatchEvents()
-    {
-        return $this->client->dispatchEvents();
-    }
+    protected $client;
 
     /**
-     * Registers a Relay event listener.
+     * Pass method calls to Relay, or the underlying client.
      *
-     * Normally `Relay::listen()` is used for this, but due to the conflict
-     * with Laravel's `Connection::listen()` this method acts as an alias.
-     *
-     * @see \Illuminate\Redis\Connections\Connection::listen()
-     *
-     * @param  callable  $callback
-     * @return bool
+     * @param  string  $method
+     * @param  array  $parameters
+     * @return mixed
      */
-    public function listenFor(callable $callback)
+    public function __call($method, $parameters)
     {
-        return $this->client->listen($callback);
-    }
+        if (method_exists($this->client, $method)) {
+            return $this->client->{$method}(...$parameters);
+        }
 
-    /**
-     * Registers an event listener for flushes.
-     *
-     * @param  callable  $callback
-     * @return bool
-     */
-    public function onFlushed(callable $callback)
-    {
-        return $this->client->onFlushed($callback);
-    }
-
-    /**
-     * Registers an event listener for invalidations.
-     *
-     * @param  callable  $callback
-     * @param  string  $pattern
-     * @return bool
-     */
-    public function onInvalidated(callable $callback, string $pattern = null)
-    {
-        return $this->client->onInvalidated($callback, $pattern);
-    }
-
-    /**
-     * Returns statistics about Relay.
-     *
-     * @return array
-     */
-    public function stats()
-    {
-        return $this->client->stats();
-    }
-
-    /**
-     * Returns information about the Relay license.
-     *
-     * @return array
-     */
-    public function license()
-    {
-        return $this->client->license();
-    }
-
-    /**
-     * Returns the connections socket identifier.
-     *
-     * @return int|false
-     */
-    public function socketId()
-    {
-        return $this->client->socketId();
-    }
-
-    /**
-     * Returns the connections socket key.
-     *
-     * Bypasses the `command()` method to avoid log spam.
-     *
-     * @return string|false
-     */
-    public function socketKey()
-    {
-        return $this->client->socketKey();
+        return parent::__call($method, $parameters);
     }
 }
