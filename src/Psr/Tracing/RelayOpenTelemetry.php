3feda79ea68ffc289ded2a44c3299975fc8d0ed3
Till Krüss <tillkruss@users.noreply.github.com>
2022-10-29 09:56:55 -0700

    Telemetry interceptors (New Relic + OpenTelemetry)

---
+++ src/Psr/Tracing/RelayOpenTelemetry.php
@@ -0,0 +1,197 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CacheWerk\Relay\Psr\Tracing;
+
+use Relay\Relay;
+use Relay\Exception;
+
+use OpenTelemetry\API\Trace\SpanKind;
+use OpenTelemetry\API\Trace\TracerInterface;
+use OpenTelemetry\API\Trace\TracerProviderInterface;
+
+class RelayOpenTelemetry
+{
+    /**
+     * The Relay instance.
+     *
+     * @var \Relay\Relay
+     */
+    protected Relay $relay;
+
+    /**
+     * The OpenTelemetry tracer provider instance.
+     *
+     * @var \OpenTelemetry\API\Trace\TracerInterface
+     */
+    protected TracerInterface $tracer;
+
+    /**
+     * Creates a new `NewRelicRelay` instance.
+     *
+     * @param  \Relay\Relay  $relay
+     * @param  \OpenTelemetry\API\Trace\TracerProviderInterface  $tracerProvider
+     * @return void
+     */
+    public function __construct(Relay $relay, TracerProviderInterface $tracerProvider)
+    {
+        $this->relay = $relay;
+        $this->tracer = $tracerProvider->getTracer('Relay', (string) phpversion('relay'));
+    }
+
+    /**
+     * Executes Relay methods inside OpenTelemetry span.
+     *
+     * @param  string  $name
+     * @param  array<mixed>  $arguments
+     * @return mixed
+     */
+    public function __call(string $name, array $arguments)
+    {
+        $span = $this->tracer->spanBuilder('Relay::' . strtolower($name))
+            ->setAttribute('db.operation', strtoupper($name))
+            ->setAttribute('db.system', 'redis')
+            ->setSpanKind(SpanKind::KIND_CLIENT)
+            ->startSpan();
+
+        try {
+            $result = $this->relay->{$name}(...$arguments);
+            $span->end();
+
+            return $result;
+        } catch (Exception $exception) {
+            $span->recordException($exception)->end();
+
+            throw $exception;
+        }
+    }
+
+    /**
+     * Executes static Relay methods.
+     *
+     * @param  string  $name
+     * @param  array<mixed>  $arguments
+     * @return mixed
+     */
+    public static function __callStatic(string $name, array $arguments)
+    {
+        return Relay::{$name}(...$arguments);
+    }
+
+    /**
+     * Scan the keyspace for matching keys inside OpenTelemetry span.
+     *
+     * @param  mixed  $iterator
+     * @param  mixed  $match
+     * @param  int  $count
+     * @param  ?string  $type
+     * @return array<mixed>|false
+     */
+    public function scan(&$iterator, $match = null, int $count = 0, ?string $type = null)
+    {
+        $span = $this->tracer->spanBuilder('Relay::scan')
+            ->setAttribute('db.operation', 'SCAN')
+            ->setAttribute('db.system', 'redis')
+            ->setSpanKind(SpanKind::KIND_CLIENT)
+            ->startSpan();
+
+        try {
+            $result = $this->relay->scan($iterator, $match, $count, $type);
+            $span->end();
+
+            return $result;
+        } catch (Exception $exception) {
+            $span->recordException($exception)->end();
+
+            throw $exception;
+        }
+    }
+
+    /**
+     * Iterates fields of Hash types inside OpenTelemetry span.
+     *
+     * @param  mixed  $key
+     * @param  mixed  $iterator
+     * @param  mixed  $match
+     * @param  int  $count
+     * @return array<mixed>|false
+     */
+    public function hscan($key, &$iterator, $match = null, int $count = 0)
+    {
+        $span = $this->tracer->spanBuilder('Relay::hscan')
+            ->setAttribute('db.operation', 'HSCAN')
+            ->setAttribute('db.system', 'redis')
+            ->setSpanKind(SpanKind::KIND_CLIENT)
+            ->startSpan();
+
+        try {
+            $result = $this->relay->hscan($key, $iterator, $match, $count);
+            $span->end();
+
+            return $result;
+        } catch (Exception $exception) {
+            $span->recordException($exception)->end();
+
+            throw $exception;
+        }
+    }
+
+    /**
+     * Iterates elements of Sets types inside OpenTelemetry span.
+     *
+     * @param  mixed  $key
+     * @param  mixed  $iterator
+     * @param  mixed  $match
+     * @param  int  $count
+     * @return array<mixed>|false
+     */
+    public function sscan($key, &$iterator, $match = null, int $count = 0)
+    {
+        $span = $this->tracer->spanBuilder('Relay::sscan')
+            ->setAttribute('db.operation', 'SSCAN')
+            ->setAttribute('db.system', 'redis')
+            ->setSpanKind(SpanKind::KIND_CLIENT)
+            ->startSpan();
+
+        try {
+            $result = $this->relay->sscan($key, $iterator, $match, $count);
+            $span->end();
+
+            return $result;
+        } catch (Exception $exception) {
+            $span->recordException($exception)->end();
+
+            throw $exception;
+        }
+    }
+
+    /**
+     * Iterates elements of Sorted Set types inside OpenTelemetry span.
+     *
+     * @param  mixed  $key
+     * @param  mixed  $iterator
+     * @param  mixed  $match
+     * @param  int  $count
+     * @return array<mixed>|false
+     */
+    public function zscan($key, &$iterator, $match = null, int $count = 0)
+    {
+        $span = $this->tracer->spanBuilder('Relay::zscan')
+            ->setAttribute('db.operation', 'ZSCAN')
+            ->setAttribute('db.system', 'redis')
+            ->setSpanKind(SpanKind::KIND_CLIENT)
+            ->startSpan();
+
+        try {
+            $result = $this->relay->zscan($key, $iterator, $match, $count);
+            $span->end();
+
+            return $result;
+        } catch (Exception $exception) {
+            $span->recordException($exception)->end();
+
+            throw $exception;
+        }
+    }
+}
