2e241a5df755b521b653a38035b8fa0011d3e100
Till Krüss <till@kruss.io>
2023-03-08 13:52:00 -0800

    intercept specific non-redis methods only

--- src/Laravel/RelayConnection.php
+++ src/Laravel/RelayConnection.php
@@ -20,18 +20,122 @@ class RelayConnection extends PhpRedisConnection implements Connection
     protected $client;
 
     /**
-     * Pass method calls to Relay, or the underlying client.
+     * Registers a new `flushed` event listener.
      *
-     * @param  string  $method
-     * @param  array<mixed>  $parameters
-     * @return mixed
+     * @param  callable  $callback
+     * @return bool
      */
-    public function __call($method, $parameters)
+    public function onFlushed(?callable $callback)
     {
-        if (method_exists($this->client, $method)) {
-            return $this->client->{$method}(...$parameters);
-        }
+        return $this->client->onFlushed($callback);
+    }
 
-        return parent::__call($method, $parameters);
+    /**
+     * Registers a new `invalidated` event listener.
+     *
+     * @param  callable  $callback
+     * @param  string  $pattern
+     * @return bool
+     */
+    public function onInvalidated(?callable $callback, ?string $pattern = null)
+    {
+        return $this->client->onInvalidated($callback, $pattern);
+    }
+
+    /**
+     * Dispatches all pending events.
+     *
+     * @return int|false
+     */
+    public function dispatchEvents()
+    {
+        return $this->client->dispatchEvents();
+    }
+
+    /**
+     * Adds ignore pattern(s). Matching keys will not be cached in memory.
+     *
+     * @param  string  $pattern,...
+     * @return int
+     */
+    public function addIgnorePatterns(string ...$pattern)
+    {
+        return $this->client->addIgnorePatterns(...$pattern);
+    }
+
+    /**
+     * Adds allow pattern(s). Only matching keys will be cached in memory.
+     *
+     * @param  string  $pattern,...
+     * @return int
+     */
+    public function addAllowPatterns(string ...$pattern)
+    {
+        return $this->client->addAllowPatterns(...$pattern);
+    }
+
+    /**
+     * Returns the connection's endpoint identifier.
+     *
+     * @return string|false
+     */
+    public function endpointId()
+    {
+        return $this->client->endpointId();
+    }
+
+    /**
+     * Returns a unique representation of the underlying socket connection identifier.
+     *
+     * @return string|false
+     */
+    public function socketId()
+    {
+        return $this->client->socketId();
+    }
+
+    /**
+     * Returns information about the license.
+     *
+     * @return array
+     */
+    public function license()
+    {
+        return $this->client->license();
+    }
+
+    /**
+     * Returns statistics about Relay.
+     *
+     * @return array
+     */
+    public function stats()
+    {
+        return $this->client->stats();
+    }
+
+    /**
+     * Returns the number of bytes allocated, or `0` in client-only mode.
+     *
+     * @return int
+     */
+    public function maxMemory()
+    {
+        return $this->client->maxMemory();
+    }
+
+    /**
+     * Flushes Relay's in-memory cache of all databases.
+     * When given an endpoint, only that connection will be flushed.
+     * When given an endpoint and database index, only that database
+     * for that connection will be flushed.
+     *
+     * @param  int  $db
+     * @param  string|null  $endpointId
+     * @param  int|null  $db
+     */
+    public function flushMemory(?string $endpointId = null, int $db = null)
+    {
+        return $this->client->flushMemory($endpointId, $db);
     }
 }
