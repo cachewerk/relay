3a5b3717db9091c28661e6ea76308af30f09b172
Till Krüss <till@kruss.io>
2023-04-20 09:46:20 -0700

    overhaul relay fpm helper

--- resources/relay-fpm.php
+++ resources/relay-fpm.php
@@ -18,6 +18,11 @@ function pct(float $a, float $b): string {
         : '0%';
 }
 
+function humansize($size, $precision = 2) {
+    for($i = 0; ($size / 1024) > 0.9; $i++, $size /= 1024) {}
+    return round($size, $precision) . ' ' . ['B', 'KB', 'MB', 'GB', 'TB'][$i];
+}
+
 ?><!doctype html>
 <html lang="en">
 <head>
@@ -25,7 +30,7 @@ function pct(float $a, float $b): string {
     <title>Relay v<?php echo phpversion('relay'); ?></title>
     <meta http-equiv="refresh" content="<?php echo $refresh; ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
     <div class="container max-w-6xl mx-auto p-8">
@@ -50,24 +55,24 @@ function pct(float $a, float $b): string {
                         <tr>
                             <td class="py-1 pr-2 border-b border-gray-200">maxmemory</td>
                             <td class="py-1 pr-2 border-b border-gray-200 text-right">
-                                <code class="text-purple-600"><?php echo number_format($maxmem); ?></code>
+                                <code class="text-purple-600"><?php echo humansize($maxmem); ?></code>
                             </td>
                             <td class="py-1 pr-2 border-b border-gray-200 text-right" colspan="2"></td>
                         </tr>
                         <tr>
                             <td class="py-1 pr-2 border-b border-gray-200">limit</td>
                             <td class="py-1 pr-2 border-b border-gray-200 text-right">
-                                <code class="text-purple-600"><?php echo number_format($mem['limit']); ?></code>
+                                <code class="text-purple-600"><?php echo humansize($mem['limit']); ?></code>
                             </td>
                             <td class="py-1 pr-2 border-b border-gray-200 text-right" colspan="2"></td>
                         </tr>
                         <tr>
                             <td class="py-1 pr-2 border-b border-gray-200">used</td>
                             <td class="py-1 pr-2 border-b border-gray-200 text-right">
-                                <code class="text-purple-600"><?php echo number_format($mem['used']); ?></code>
+                                <code class="text-purple-600"><?php echo humansize($mem['used']); ?></code>
                             </td>
                             <td class="py-1 pr-2 border-b border-gray-200 text-right">
-                                <code class="text-purple-600"><?php echo number_format($mem['total']); ?></code>
+                                <code class="text-purple-600"><?php echo humansize($mem['total']); ?></code>
                             </td>
                             <td class="py-1 pr-2 border-b border-gray-200 text-right">
                                 <code class="text-purple-600"><?php echo pct($mem['used'], $mem['total']) ?></code>
@@ -77,41 +82,16 @@ function pct(float $a, float $b): string {
                         <tr>
                             <td class="py-1 pr-2 border-b border-gray-200">active</td>
                             <td class="py-1 pr-2 border-b border-gray-200 text-right">
-                                <code class="text-purple-600"><?php echo number_format($mem['active']); ?> </code>
+                                <code class="text-purple-600"><?php echo humansize($mem['active']); ?> </code>
                             </td>
                             <td class="py-1 pr-2 border-b border-gray-200 text-right">
-                                <code class="text-purple-600"><?php echo number_format($mem['limit']); ?></code>
+                                <code class="text-purple-600"><?php echo humansize($mem['limit']); ?></code>
                             </td>
                             <td class="py-1 pr-2 border-b border-gray-200 text-right">
                                 <code class="text-purple-600"><?php echo pct($mem['active'], $mem['limit']); ?></code>
                                 <meter value="<?php echo $mem['active']; ?>" min="0" max="<?php echo $mem['limit']; ?>">
                             </td>
                         </tr>
-                    </tbody>
-                </table>
-
-            </div>
-
-            <div class="flex-grow w-full lg:w-2/5">
-
-                <h2 class="text-2xl font-extrabold text-gray-900 tracking-tight mb-4">
-                    Process
-                </h2>
-
-                <table class="w-full text-left border-collapse mb-12">
-                    <thead class="text-sm font-semibold text-gray-600 pb-2 pr-2 border-b-2 border-gray-200">
-                        <td class="py-1 pr-2 border-b border-gray-200">ID</td>
-                        <td class="py-1 pr-2 border-b border-gray-200 text-right">Value</td>
-                    </thead>
-                    <tbody class="text-sm">
-                        <?php foreach ($ids as $key => $val) : ?>
-                            <tr>
-                                <td class="py-1 pr-2 border-b border-gray-200"><?php echo $key; ?></td>
-                                <td class="py-1 pr-2 border-b border-gray-200 text-right">
-                                    <code class="text-purple-600"><?php echo $val; ?></code>
-                                </td>
-                            </tr>
-                        <?php endforeach; ?>
                     </tbody>
                 </table>
 
@@ -171,13 +151,24 @@ function pct(float $a, float $b): string {
                             'ops_per_sec',
                             'bytes_sent',
                             'bytes_received',
-                            'walltime',
+                            'command_usec',
+                            'rinit_usec',
+                            'rshutdown_usec',
+                            'sigio_usec',
                         ] as $name) : ?>
                             <tr>
-                                <td class="py-1 pr-2 border-b border-gray-200"><?php echo $name; ?></td>
+                                <td class="py-1 pr-2 border-b border-gray-200">
+                                    <?php echo $name; ?>
+                                </td>
                                 <td class="py-1 pr-2 border-b border-gray-200 text-right">
                                     <code class="text-purple-600">
-                                        <?php echo number_format($stats[$name]); ?>
+                                        <?php if (strpos($name, 'bytes_') === 0) : ?>
+                                            <?php echo humansize($stats[$name]); ?>
+                                        <?php elseif (strpos($name, '_usec')) : ?>
+                                            <?php echo number_format($stats[$name] / 1000, 2); ?> ms
+                                        <?php else : ?>
+                                            <?php echo number_format($stats[$name]); ?>
+                                        <?php endif; ?>
                                     </code>
                                 </td>
                             </tr>
@@ -189,6 +180,27 @@ function pct(float $a, float $b): string {
             </div>
 
             <div class="flex-grow w-full lg:w-2/5">
+
+                <h2 class="text-2xl font-extrabold text-gray-900 tracking-tight mb-4">
+                    Process
+                </h2>
+
+                <table class="w-full text-left border-collapse mb-12">
+                    <thead class="text-sm font-semibold text-gray-600 pb-2 pr-2 border-b-2 border-gray-200">
+                        <td class="py-1 pr-2 border-b border-gray-200">ID</td>
+                        <td class="py-1 pr-2 border-b border-gray-200 text-right">Value</td>
+                    </thead>
+                    <tbody class="text-sm">
+                        <?php foreach ($ids as $key => $val) : ?>
+                            <tr>
+                                <td class="py-1 pr-2 border-b border-gray-200"><?php echo $key; ?></td>
+                                <td class="py-1 pr-2 border-b border-gray-200 text-right">
+                                    <code class="text-purple-600"><?php echo $val; ?></code>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
 
                 <h2 class="text-2xl font-extrabold text-gray-900 tracking-tight mb-4">
                     Errors
@@ -225,6 +237,48 @@ function pct(float $a, float $b): string {
             <div class="flex-grow w-full lg:w-2/5">
 
                 <h2 class="text-2xl font-extrabold text-gray-900 tracking-tight mb-4">
+                    License
+                </h2>
+
+                <table class="w-full text-left border-collapse mb-12">
+                    <thead class="text-sm font-semibold text-gray-600 pb-2 pr-2 border-b-2 border-gray-200">
+                        <td class="py-1 pr-2 border-b border-gray-200">Key</td>
+                        <td class="py-1 pr-2 border-b border-gray-200 text-right">Value</td>
+                    </thead>
+                    <tbody class="text-sm">
+                        <?php foreach ([
+                            'state',
+                            'reason',
+                            'memory-limit',
+                            'response-code',
+                            'response-id',
+                            'timestamp',
+                            'attempts',
+                            'errors',
+                        ] as $name) : ?>
+                            <tr>
+                                <td class="py-1 pr-2 border-b border-gray-200"><?php echo $name; ?></td>
+                                <td class="py-1 pr-2 border-b border-gray-200 text-right">
+                                    <code class="text-purple-600">
+                                        <?php if ($name === 'timestamp') : ?>
+                                            <time datetime="<?php echo date('c', $license[$name]); ?>" title="<?php echo date('r', $license[$name]); ?>">
+                                                <?php echo $license[$name]; ?>
+                                            </time>
+                                        <?php else : ?>
+                                            <?php echo $license[$name]; ?>
+                                        <?php endif; ?>
+                                    </code>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+
+            </div>
+
+            <div class="flex-grow w-full lg:w-2/5">
+
+                <h2 class="text-2xl font-extrabold text-gray-900 tracking-tight mb-4">
                     Instances
                 </h2>
 
@@ -251,39 +305,6 @@ function pct(float $a, float $b): string {
                                     <code class="text-purple-600"><?php echo number_format(
                                         array_sum(array_map(fn ($conn) => array_sum($conn['keys']), $info['connections']))
                                     ); ?></code>
-                                </td>
-                            </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
-
-            </div>
-
-            <div class="w-full lg:w-2/5">
-
-                <h2 class="text-2xl font-extrabold text-gray-900 tracking-tight mb-4">
-                    License
-                </h2>
-
-                <table class="w-full text-left border-collapse mb-12">
-                    <thead class="text-sm font-semibold text-gray-600 pb-2 pr-2 border-b-2 border-gray-200">
-                        <td class="py-1 pr-2 border-b border-gray-200">Key</td>
-                        <td class="py-1 pr-2 border-b border-gray-200 text-right">Value</td>
-                    </thead>
-                    <tbody class="text-sm">
-                        <?php foreach ([
-                            'state',
-                            'memory-limit',
-                            'reason',
-                            'code',
-                            'x-resp-id',
-                        ] as $name) : ?>
-                            <tr>
-                                <td class="py-1 pr-2 border-b border-gray-200"><?php echo $name; ?></td>
-                                <td class="py-1 pr-2 border-b border-gray-200 text-right">
-                                    <code class="text-purple-600">
-                                        <?php echo $license[$name]; ?>
-                                    </code>
                                 </td>
                             </tr>
                         <?php endforeach; ?>
