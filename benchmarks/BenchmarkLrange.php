8e3b571728f5194186e3687f38f428c008c4a750
michael-grunder <michael.grunder@gmail.com>
2023-07-04 11:04:36 -0700

    Add LRANGE benchmark

---
+++ benchmarks/BenchmarkLrange.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace CacheWerk\Relay\Benchmarks;
+
+class BenchmarkLrange extends Support\Benchmark {
+    /**
+     * @var array<int, string>
+     */
+    protected array $keys;
+
+    public function getName(): string {
+        return 'LRANGE';
+    }
+
+    public function seedKeys(): void {
+        $redis = $this->createPredis();
+
+        foreach ($this->loadJsonFile('meteorites.json', true) as $item) {
+            $redis->rpush((string)$item['id'], $this->flattenArray($item));
+            $this->keys[] = $item['id'];
+        }
+    }
+
+    public function setUp(): void
+    {
+        $this->flush();
+        $this->setUpClients();
+        $this->seedKeys();
+    }
+
+    /** @phpstan-ignore-next-line */
+    protected function runBenchmark($client): int {
+        foreach ($this->keys as $key) {
+            $client->lrange($key, 0, -1);
+        }
+
+        return count($this->keys);
+    }
+
+    public function benchmarkPredis(): int {
+        return $this->runBenchmark($this->predis);
+    }
+
+    public function benchmarkPhpRedis(): int {
+        return $this->runBenchmark($this->phpredis);
+    }
+
+    public function benchmarkRelayNoCache(): int {
+        return $this->runBenchmark($this->relayNoCache);
+    }
+
+    public function benchmarkRelay(): int {
+        return $this->runBenchmark($this->relay);
+    }
+}
