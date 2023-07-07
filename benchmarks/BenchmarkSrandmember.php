e409594a3dc778401bc1c7945135247cfcd9fb0f
michael-grunder <michael.grunder@gmail.com>
2023-07-06 22:26:42 -0700

    Add SRANDMEMBER benchmark.

---
+++ benchmarks/BenchmarkSrandmember.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace CacheWerk\Relay\Benchmarks;
+
+class BenchmarkSrandmember extends Support\BenchmarkSetCommand {
+    public function getName(): string {
+        return 'SRANDMEMBER';
+    }
+
+    public static function flags(): int {
+        return self::SET | self::READ;
+    }
+
+    /** @phpstan-ignore-next-line */
+    protected function runBenchmark($client): int {
+        foreach ($this->keys as $key) {
+            $client->srandmember($key);
+        }
+
+        return count($this->keys);
+    }
+
+    public function benchmarkPredis(): int {
+        return $this->runBenchmark($this->predis);
+    }
+
+    public function benchmarkPhpRedis(): int {
+        return $this->runBenchmark($this->phpredis);
+    }
+
+    public function benchmarkRelayNoCache(): int {
+        return $this->runBenchmark($this->relayNoCache);
+    }
+
+    public function benchmarkRelay(): int {
+        return $this->runBenchmark($this->relay);
+    }
+}
