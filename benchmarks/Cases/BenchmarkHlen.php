03bf1dfbce810067be26c5d2f797dda987c550f9
michael-grunder <michael.grunder@gmail.com>
2023-07-20 08:47:59 -0700

    Include every "length" command + generic way to set typed keys
    * Relay caches in-memory the "length" information about keys of any
      type, so we should include all of these commands in our benchmark
      suite.
    
    * Add a generic helper method that uses our flags to set keys of a given
      type in a generic way.

--- benchmarks/Cases/BenchmarkHlen.php
+++ benchmarks/Cases/BenchmarkHlen.php
@@ -4,10 +4,10 @@ namespace CacheWerk\Relay\Benchmarks\Cases;
 
 use CacheWerk\Relay\Benchmarks\Support\BenchmarkLenCommand;
 
-class BenchmarkSCARD extends BenchmarkLenCommand
+class BenchmarkHLEN extends BenchmarkLenCommand
 {
     public static function flags(): int
     {
-        return self::SET | self::READ;
+        return self::HASH | self::READ;
     }
 }
