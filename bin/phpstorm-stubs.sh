da78e3e1f2531ddf71be34f430f2a5efed94fd2a
Till Krüss <till@kruss.io>
2025-10-03 10:02:18 -0700

    fix indentation

--- bin/phpstorm-stubs.sh
+++ bin/phpstorm-stubs.sh
@@ -3,8 +3,8 @@
 set -e
 
 if [ $# -ne 1 ]; then
-  echo "Usage: $0 <version>"
-  exit 1
+    echo "Usage: $0 <version>"
+    exit 1
 fi
 
 VERSION="$1"
@@ -15,17 +15,17 @@ echo "Downloading ${VERSION} stubs..."
 curl -s -o "relay.stub.php" "$URL"
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  SED="gsed"
+    SED="gsed"
 else
-  SED="sed"
+    SED="sed"
 fi
 
 echo "Adjusting stubs..."
 
 "$SED" -i -E \
-  -e 's/^(.*\bfunction\b.*);/\1 {}/' \
-  -e 's/^(\s*)\);$/\1) {}/'\
-  -e 's/^(\s*)\): ([^;]+);$/\1): \2 {}/' \
-  -e 's/^(\s+\*) @alias (.+)$/\1 @see \2()/' \
-  -e 's/^(\s+\*) @var \$context array/\1 @example $context array/' \
-  relay.stub.php
+    -e 's/^(.*\bfunction\b.*);/\1 {}/' \
+    -e 's/^(\s*)\);$/\1) {}/'\
+    -e 's/^(\s*)\): ([^;]+);$/\1): \2 {}/' \
+    -e 's/^(\s+\*) @alias (.+)$/\1 @see \2()/' \
+    -e 's/^(\s+\*) @var \$context array/\1 @example $context array/' \
+    relay.stub.php
